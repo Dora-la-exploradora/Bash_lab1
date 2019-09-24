fc509881060f303eecc49c565c7d018832c6be01
Dora-la-exploradora <55231190+Dora-la-exploradora@users.noreply.github.com>
2019-09-24 22:45:28 +0300

    Update Bash1.sh

--- Bash1.sh
+++ Bash1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo 'Введите название папки:' 
 read namedir
-echo -e "path \tname \t extension \t size(bytes) \t last_modification_date \t duration" > Result.xls
-IFS=$'\n'
+echo -e "path \tname \t extension \t size(bytes) \t last_modification_date \t duration" > Result.xls	#создание таблицы
+IFS=$'\n'	#чтобы читались файлы с пробелами в названии
 dir=$namedir
-function task {				
+function  func {				
 for f in "$dir"/*			
 do
-	if test -d "$f" ; then 
+	if test -d "$f" ; then # для рекурсивного вывода
 		dir=$f	
-		task	
+		func	
 	else	
-		local file="${f##*/}"
+		local file="${f##*/}"	#выделяет название файла из пути
 			
-		local name="${file%.*}"
+		local name="${file%.*}"	
 
 		local extension="${file:${#name} + 1}"
 
@@ -21,15 +21,15 @@ do
 
 		local size=$(du -b "$f" | awk '{print $1}') 
 
-		if [[ "$f" = *.MOV || "$f" = *.mp3 || "$f" = *.avi || "$f" = *.mp4 ]]
+		if [[ "$f" = *.MOV || "$f" = *.mp3 || "$f" = *.avi || "$f" = *.mp4 ]]		#если это видео/аудио файл, выводит длину
 		then
 			local duration=$(ffprobe -i "$f" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal);
 		else
 			local duration=$(echo --);
 		fi
-		echo -e "$f \t$name \t$extension \t$size \t$date \t$duration" >> Result.xls		
+		echo -e "$f \t$name \t$extension \t$size \t$date \t$duration" >> Result.xls		#вывод в файл	
 		dir=$namedir	
 	fi
 done
 }
-task 
+func
